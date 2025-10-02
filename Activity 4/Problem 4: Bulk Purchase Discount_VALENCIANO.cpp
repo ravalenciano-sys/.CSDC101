d9333b312f80872dd6964aedb8e7db4fc9d54f0f
ravalenciano-sys <ravalenciano@gbox.adnu.edu.ph>
2025-10-01 21:33:07 -0700

    Problem 4: Bulk Purchase Discount_VALENCIANO.cpp

---
+++ Activity 4/Problem 4: Bulk Purchase Discount_VALENCIANO.cpp
@@ -0,0 +1,23 @@
+Problem 4: Bulk Purchase Discount
+
+Problem:
+Customers get discounts based on how many cups they buy:
+1–4 cups: No discount
+5–9 cups: 10% discount
+10+ cups: 20% discount
+Each cup is $1.
+
+Task:
+
+Write a program that:
+Ask how many cups the customer wants.
+Calculates the total cost with the correct discount.
+Outputs the final price.
+
+Sample Input:
+How many cups? 7
+
+Sample Output:
+Total cost: $6.30
+
+  
