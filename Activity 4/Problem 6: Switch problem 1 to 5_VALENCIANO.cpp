a3c8005f94b50cc95695bd6766bf64fa1306f741
ravalenciano-sys <ravalenciano@gbox.adnu.edu.ph>
2025-10-01 21:34:16 -0700

    Problem 6: Switch problem 1 to 5_VALENCIANO.cpp

---
+++ Activity 4/Problem 6: Switch problem 1 to 5_VALENCIANO.cpp
@@ -0,0 +1,10 @@
+Problem 6: Switch problem 1 to 5
+Combine all problems using switch statement
+
+Sample Input:
+What problem do you want to see? 1
+
+Sample Output:
+*Basic pricing Decision Runs*
+
+Submit a cpp file, a link to your github repository, or a notion page.
