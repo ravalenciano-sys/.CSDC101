c7ef66d9508b637c5f78511aa0c4f58eb74ccfd5
ravalenciano-sys <ravalenciano@gbox.adnu.edu.ph>
2025-10-01 21:33:45 -0700

    Problem 5: Player Movement_VALENCIANO.cpp

---
+++ Activity 4/Problem 5: Player Movement_VALENCIANO.cpp
@@ -0,0 +1,21 @@
+Problem:
+The player moves depending on the pressed button. Player starts at (0,0).
+
+Task:
+
+
+Write a program that:
+Lets the player change coordinates depending on the button pressed.
+W =  0, +1
+S = 0, -1
+A = -1,0
+D = +1,0 
+
+Sample Input:
+W
+
+
+Sample Output:
+The location of the player is ( 0, 1)
+
+
