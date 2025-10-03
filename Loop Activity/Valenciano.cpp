45223833df13212cce019b217cc0093cf5825192
ravalenciano-sys <ravalenciano@gbox.adnu.edu.ph>
2025-10-02 22:46:03 -0700

    Triangle 1 Valenciano.cpp

---
+++ Loop Activity/Valenciano.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std; 
+
+int main() { 
+    int height;  
+    
+    cout << "Enter the height (and base) of the triangle: "; 
+    cin >> height; 
+    
+    
+    for (int i = 1; i <= height; i++) { 
+       for (int j = 1; j <= i; j++) {
+            cout << "* " ; 
+        }
+        cout << endl; 
+    } 
+     
+    return 0; 
+} 
