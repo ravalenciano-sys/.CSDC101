b98bcde38c8a466624d3df04634fc63918360a27
ravalenciano-sys <ravalenciano@gbox.adnu.edu.ph>
2025-10-04 14:48:27 +0800

    Update Problem 6: Switch problem 1 to 5_VALENCIANO.cpp

--- Activity 4/Problem 6: Switch problem 1 to 5_VALENCIANO.cpp
+++ Activity 4/Problem 6: Switch problem 1 to 5_VALENCIANO.cpp
@@ -8,3 +8,38 @@ Sample Output:
 *Basic pricing Decision Runs*
 
 Submit a cpp file, a link to your github repository, or a notion page.
+
+
+  #include <iostream> 
+using namespace std; 
+
+int main() { 
+    
+    int choice; 
+    
+    cout << "What problem do you want to see? ";
+    cin >> choice; 
+    
+    switch (choice) { 
+        case 1: 
+            cout << "Basic Pricing Decision Runs" << endl; 
+            break; 
+        case 2: 
+            cout << "Inventory Management Simulation Runs" << endl; 
+            break; 
+        case 3: 
+            cout << "Employee Scheduling Algorithm Runs" << endl; 
+            break; 
+        case 4: 
+            cout << "Customer Feedback Analysis Start" << endl; 
+            break; 
+        case 5: 
+            cout <<"Sales Forecasting Tool Executes" << endl; 
+            break; 
+        default: 
+            cout << "Invalid choice. Please select problem from 1 to 5 only." << endl; 
+    } 
+    
+    return 0; 
+    
+} 
