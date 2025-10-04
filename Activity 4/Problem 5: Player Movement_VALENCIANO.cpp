4982b903a261e95d33279c5000aaf054127f1879
ravalenciano-sys <ravalenciano@gbox.adnu.edu.ph>
2025-10-04 14:39:25 +0800

    Problem 5: Player Movement_VALENCIANO.cpp

--- Activity 4/Problem 5: Player Movement_VALENCIANO.cpp
+++ Activity 4/Problem 5: Player Movement_VALENCIANO.cpp
@@ -19,3 +19,34 @@ Sample Output:
 The location of the player is ( 0, 1)
 
 
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{ 
+    
+    int x = 0, y = 0; 
+    char move; 
+    
+    cout << "Enter movement key (W/A/S/D): "; 
+    cin >> move; 
+    move = toupper(move); 
+    
+    if (move == 'W') { 
+        y += 1; 
+    } else if (move == 'S') { 
+        y -= 1; 
+    } else if (move == 'A') { 
+        x -= 1; 
+    } else if (move == 'D') { 
+        x += 1; 
+    } else { 
+        cout << "Invalid Input" << endl; 
+        
+    return 1; 
+    } 
+    
+    cout << "The location of the player is (" << x << ", " << y << " )" << endl; 
+    
+    return 0; 
+} 
